f23d52dbf9fa1ae40a89f74a361e94c678de5015
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-08-14 11:22:26 +0530

    Create java55.java

---
+++ java55.java
@@ -0,0 +1,42 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str1=null;
+		String str2=null;
+
+		Scanner sc=new Scanner(System.in);
+		str1=sc.nextLine();
+		str2=sc.nextLine();
+		str1=str1.toLowerCase();
+		str2=str2.toLowerCase();
+		if(str1.length()!=str2.length()){
+		System.out.println("no");
+			return;
+		}
+		
+		boolean flg=true;
+		for(int i=0; i<str1.length();i++){
+			if(str1.charAt(i)!=str2.charAt(i)){
+				flg=false;
+				break;
+			}
+		}
+		
+		if(flg){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+
+	}
+}
