c1d3b2530e7be71687c6fb1bc2a04b69b33e9e27
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-08-30 11:57:53 +0530

    Create java18.java

---
+++ java18.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		String str[]=new String[n];
+		for(int i=0;i<n;i++)
+		{
+			str[i]=s.next();
+		}
+		String k="kabali";
+		char kc[]=k.toCharArray();
+		Arrays.sort(kc);
+		int cnt=0;
+		for(int i=0;i<n;i++)
+		{
+			char ch[]=new char[str[i].length()];
+			ch=str[i].toLowerCase().toCharArray();
+			Arrays.sort(ch);
+			if(Arrays.equals(kc,ch))
+			{
+			 cnt++;
+			}
+		}
+		System.out.print(cnt);
+
+	}
+}
